afa4ab60251c81e28ce5fa0cadfad68d7d9630c0
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-09 16:34:17 +0100

    windows gdi calculator

---
+++ gdi_calc.py
@@ -0,0 +1,59 @@
+import os
+from Tkinter import *
+
+t=1
+root=Tk()
+root.title("calculator")
+root.geometry("500x300")
+frame=Frame(root)
+frame2=Frame(root)
+global ss
+ss=StringVar()
+ss.set(" ")
+iiput=Text(frame2)
+iiput2=Entry(frame)
+
+
+def ups():
+    s=str(eval(iiput2.get()))
+    sss=iiput2.get()
+    ss=sss + " = " + s + "\n"
+    iiput.insert(INSERT,ss)
+    return 
+
+
+iiput.width=3
+iiput.height=1
+iiput2.width=3
+iiput2.height=1
+frame.pack()
+frame.width=500
+frame.height=80
+frame2.pack(side =BOTTOM)
+frame2.width=500
+frame2.height=200
+sups=Button(frame,text="calc",command=ups)
+sups.pack(side =LEFT) 
+iiput2.pack(side =LEFT) 
+iiput.pack(side =BOTTOM) 
+root.mainloop()            
+t=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
