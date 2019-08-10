cd0704b98c649263256d9458ffd2ffeacc960fab
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-10 21:29:35 +0100

    string function user define

---
+++ stringfunc.py
@@ -0,0 +1,14 @@
+
+def string ( toadd , toadds ):
+    tadd=""
+    for n in range (0,toadd):
+        tadd=tadd + toadds
+    return tadd
+
+
+
+for nn in range (1,18):
+    print (string(nn,"*"))
+
+
+
