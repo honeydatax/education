7766fde8f7410d53ef2bf1fa61c17c7fa10b2f50
lisbon <16432726+honeydatax@users.noreply.github.com>
2019-08-11 12:24:07 +0100

    python py extensions

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
-print "python -m SimpleHTTPServer  8080"
-print "http://localhost:8080/server.py"
+#!/usr/bin/python
+print "Content-type:text/text\r\n\r\n"
+print "python -m CGIHTTPServer 8080"
+print "mkdir cgi-bin"
+print "chmod 755 cgi-bin/*.py"
+print "http://localhost:8080/cgi-bin/server.py"
 print "web html server .!"
